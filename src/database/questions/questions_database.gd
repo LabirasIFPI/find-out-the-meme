390447a0b79dd9fe0f143e34a6f7c2bf6b1c486c
Diego Araújo <diesgo.araujo@gmail.com>
2024-01-25 10:32:44 -0300

    Feat: added the database for questions and answers, the ground with image and the big screen with image

---
+++ src/database/questions/questions_database.gd
@@ -0,0 +1,17 @@
+const questionsDatabase = [
+{
+	"name": "muricoca",
+	"image_src": "res://src/database/questions/images/muricoca.jpeg",
+	"answer_name": "faz_pic"
+},
+{
+	"name": "matematica",
+	"image_src": "res://src/database/questions/images/matematica.jpeg",
+	"answer_name": "nazare"
+},
+{
+	"name": "manga",
+	"image_src": "res://src/database/questions/images/manga.jpeg",
+	"answer_name": "ze_da_manga"
+}
+]
